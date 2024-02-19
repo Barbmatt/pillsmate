bd4168b274d3f5425709d424c77275973dc8bc72
Bárbara Mattioli <barbaramattioli@outlook.com>
2024-02-19 20:10:04 -0300

    Feat: Set periods on Calendar

--- App.tsx
+++ App.tsx
@@ -2,26 +2,51 @@ import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { Calendar, DateData } from "react-native-calendars";
+import { MarkedDates } from "react-native-calendars/src/types";
 
 export default function App() {
   const [selectedStartingDay, setSelectedStartingDay] = useState<string>("");
   const [selectedEndingDay, setSelectedEndingDay] = useState<string>("");
+  const [modifyStartDate, setModifyStartDate] = useState<boolean>(true);
 
   const setPeriod = (selectedDay: DateData) => {
-    if (selectedStartingDay === "")
+    if (modifyStartDate) {
       setSelectedStartingDay(selectedDay.dateString);
-    else setSelectedEndingDay(selectedDay.dateString);
+      setSelectedEndingDay("");
+      setModifyStartDate(false);
+    } else {
+      if (selectedStartingDay >= selectedDay.dateString) {
+        setSelectedEndingDay(selectedStartingDay);
+        setSelectedStartingDay(selectedDay.dateString);
+      } else setSelectedEndingDay(selectedDay.dateString);
+      setModifyStartDate(true);
+    }
   };
+
+  const completePeriod = (): MarkedDates => {
+    const middleDates: MarkedDates = {};
+    let date = new Date(new Date(selectedStartingDay).getTime() + 86400000);
+    let endDate = new Date(selectedEndingDay);
+    while (date < endDate) {
+      middleDates[date.toISOString().substring(0, 10)] = { color: "lightblue" };
+      date = new Date(date.getTime() + 86400000);
+    }
+    return middleDates;
+  };
+
+  const middleDates: MarkedDates =
+    selectedEndingDay !== "" ? completePeriod() : {};
 
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Pillsmate</Text>
-      <Text>Mark the Start Date and End Date of your blister:</Text>
+      <Text>Mark the period for the next blister:</Text>
       <Calendar
         markingType={"period"}
         onDayPress={(selectedDay) => setPeriod(selectedDay)}
         markedDates={{
           [selectedStartingDay]: { startingDay: true, color: "lightblue" },
+          ...middleDates,
           [selectedEndingDay]: { endingDay: true, color: "lightblue" },
         }}
       ></Calendar>
